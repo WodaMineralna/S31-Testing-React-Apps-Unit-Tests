5c2ddd0250099be574f201b02b10759c3cc1706a
WodaMineralna <raknieborak65@gmail.com>
2025-07-03 23:07:03 +0200

    Added Test Suites grouping, using describe() global function

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,15 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("Renders 'Hello World' as a text", () => {
-  // ! 1. Arrange
-  render(<Greeting />);
+// * describe(descOfThe'Category', testFn)
+describe("<Greeting /> Component", () => {
+  test("Renders 'Hello World' as a text", () => {
+    // ! 1. Arrange
+    render(<Greeting />);
 
-  // ! 2. Act
-  // ... nothing
+    // ! 2. Act
+    // ... nothing
 
-  // ! 3. Assert
-  // ^ { exact: true } - (by default)
-  const helloWorldElement = screen.getByText("Hello World!");
-  expect(helloWorldElement).toBeInTheDocument();
+    // ! 3. Assert
+    // ^ { exact: true } - (by default)
+    const helloWorldElement = screen.getByText("Hello World!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
